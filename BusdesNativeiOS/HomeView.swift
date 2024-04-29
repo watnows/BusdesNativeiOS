1e29bc60e45cdfbf2cb7dc6f77889df187f78154
RyunosukeKurokawa <kuroryu.basket@gmail.com>
2024-04-29 15:37:41 +0900

    add HomeView

---
+++ BusdesNativeiOS/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  BusdesNativeiOS
+//
+//  Created by 黒川龍之介 on 2024/04/29.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}

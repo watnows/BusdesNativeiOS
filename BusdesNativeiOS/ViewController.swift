fd17bff3f5d53123948f9ce86321e2b2d3e00aa7
RyunosukeKurokawa <kuroryu.basket@gmail.com>
2024-04-29 15:32:46 +0900

    first commit

---
+++ BusdesNativeiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BusdesNativeiOS
+//
+//  Created by 黒川龍之介 on 2024/04/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

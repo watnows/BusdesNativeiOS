0461aa3fcb898db2b3b2bdf898b7f7652dbab7aa
RyunosukeKurokawa <kuroryu.basket@gmail.com>
2024-04-30 14:03:08 +0900

    目的地設定画面のレイアウト調整

--- BusdesNativeiOS/Views/AddLine/SetGoalView.swift
+++ BusdesNativeiOS/Views/AddLine/SetGoalView.swift
@@ -1,39 +1,58 @@
-//
-//  SetGoalView.swift
-//  BusdesNativeiOS
-//
-//  Created by 黒川龍之介 on 2024/04/30.
-//
-
 import SwiftUI
 
 struct SetGoalView: View {
+    @State var selected = 0
     var body: some View {
         VStack {
             Text("どちらでバスを降りますか？")
+                .font(.headline)
+                .padding(.top, 100)
+            Spacer()
             HStack {
+                Spacer()
                 Button {
-                    MenuView()
+                    selected = 0
                 } label: {
                     Text("南草津駅")
                 }
+                .frame(width: 130, height: 40)
+                .foregroundColor(.white)
+                .background(.red) // EF3124
+                .clipShape(Capsule())
+                Spacer()
                 Button {
-                    MenuView()
+                    selected = 1
                 } label: {
                     Text("立命館大学")
                 }
+                .frame(width: 130, height: 40)
+                .foregroundColor(.white)
+                .background(.red)
+                .clipShape(Capsule())
+                 Spacer()
             }
+            .padding(.bottom, 40)
             Button {
                 MenuView()
             } label: {
                 Text("決定")
             }
+            .frame(width: 130,height:40)
+            .foregroundColor(.black)
+            .background(.gray)
+            .clipShape(Capsule())
             Button {
                 MenuView()
             } label: {
                 Text("戻る")
             }
+            .frame(width: 130, height: 40)
+            .foregroundColor(.black)
+            .background(.gray)
+            .clipShape(Capsule())
+            Spacer()
         }
+        .navigationTitle("My路線の追加")
     }
 }
 

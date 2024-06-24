c5ebee29dfc07a664c49a3bb60bbcc285f4f7b72
RyunosukeKurokawa <kuroryu.basket@gmail.com>
2024-06-24 16:01:23 +0900

    Myrouteのデータの受け渡し

--- BusdesNativeiOS/Views/SetGoal/SetGoalViewController.swift
+++ BusdesNativeiOS/Views/SetGoal/SetGoalViewController.swift
@@ -1,11 +1,15 @@
+import CoreData
 import SwiftUI
 import UIKit
 
 protocol SetGoalViewControllerProtocol: AnyObject{
     func goHome()
+    func setRoute(to: String)
+    var from: String { get set }
 }
 
 class SetGoalViewController: UIViewController {
+    var from = ""
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -29,5 +33,19 @@ class SetGoalViewController: UIViewController {
 extension SetGoalViewController: SetGoalViewControllerProtocol {
     func goHome() {
         navigationController?.popToRootViewController(animated: true)
+    }
+
+    func setRoute(to: String) {
+        if from == to {
+            let alert = UIAlertController(title: "設定エラー",
+                                          message: "乗り場と降り場が同じようです",
+                                          preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        } else {
+            //保存機能書きたい！！
+            navigationController?.popToRootViewController(animated: true)
+        }
+        print(from,to)
     }
 }
